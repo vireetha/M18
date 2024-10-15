ca3b78610c0266c5b4f11d5e72436df4252766f3
vireetha <dudalaviree69@gmail.com>
2024-10-15 12:08:12 +0530

    16

---
+++ Lesson2/HCF.py
@@ -0,0 +1,12 @@
+num1 = float(input(" Please Enter the First Value  Num1 : "))
+num2 = float(input(" Please Enter the Second Value Num2 : "))
+
+
+while(num2 != 0):
+    temp = num2
+    num2 = num1 % num2
+    num1 = temp
+
+hcf = num1   
+
+print("HCF of num1 and num2 is",hcf)
