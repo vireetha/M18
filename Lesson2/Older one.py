ee540cdf17fb57c0fecee9f755fab54ac3c4fcb2
vireetha <dudalaviree69@gmail.com>
2024-10-15 11:27:03 +0530

    5

---
+++ Lesson2/Older one.py
@@ -0,0 +1,10 @@
+print("Check your is between 10 to 20 years or not")
+x = int(input("enter your age: "))
+
+
+if x > 10: 
+  print("Your age is more than 10 years")
+  if x > 20: 
+    print("And it is more than 20 as well")
+  else:
+    print("But it is less not more than 20")
