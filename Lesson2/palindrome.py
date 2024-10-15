8a777997c6f6233ec413fd1038c498fa63ecd06b
vireetha <dudalaviree69@gmail.com>
2024-10-15 12:10:47 +0530

    17

---
+++ Lesson2/palindrome.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a number: "))
+rev = 0
+
+temp = num
+while temp>0:
+    rem = temp%10
+    rev = rem + (rev*10)
+    temp = int(temp/10)
+
+if rev==num:
+    print("\nIt is a Palindrome Number")
+else:
+    print("\nIt is not a Palindrome Number")
