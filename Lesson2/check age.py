f0744744c8bd868f08b8818d50d5a725ce28e3f2
vireetha <dudalaviree69@gmail.com>
2024-10-15 11:29:37 +0530

    ACP

---
+++ Lesson2/check age.py
@@ -0,0 +1,9 @@
+age = int(input("how old are you?:"))
+if age >10:
+    if age>=10 and age<=20:
+        print("you are allowed in class age limit in between 10 and 20")
+    else:
+        print("you are not allowed in class")
+else:
+    print("you are too young for the class")
+    
