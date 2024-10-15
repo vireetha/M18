1d9891ae9046265306a87dc170ff7145afa7789b
vireetha <dudalaviree69@gmail.com>
2024-10-15 11:36:48 +0530

    7

---
+++ Lesson2/Sum of whole numbers.py
@@ -0,0 +1,9 @@
+age=int(input("Enter your age:"))
+if age>=10:
+  if age<=20:
+    print ("Your age is : ", age," \n You are allowed to enter the class")
+  else:
+    print ("Your age is:",age , " \nYou are not allowed to enter the class")
+else:
+  print("Your age is:",age, " \n You are not eligible")
+
